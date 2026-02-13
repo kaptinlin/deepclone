e0d9e1fb394ca51bea99168d3a5b290a3f4f1d3e
kaptinlin <kaptinlin@gmail.com>
2026-02-13 20:33:13 +0800

    feat: upgrade to Go 1.26 with modern idioms
    - Update go.mod to Go 1.26 in main package and benchmarks
    - Modernize benchmarks to use b.Loop() (Go 1.24+) instead of for i := 0; i < b.N; i++
    - Replace interface{} with any throughout benchmarks
    - Update init function to use for range N syntax (Go 1.22+)
    - All tests and lints pass successfully

--- benchmarks/benchmark_test.go
+++ benchmarks/benchmark_test.go
@@ -26,7 +26,7 @@ type NestedStruct struct {
 	Name     string
 	Profile  *Profile
 	Tags     []string
-	Settings map[string]interface{}
+	Settings map[string]any
 }
 
 type Profile struct {
@@ -67,7 +67,7 @@ var (
 			},
 		},
 		Tags:     []string{"tag1", "tag2", "tag3"},
-		Settings: map[string]interface{}{"key1": "value1", "key2": 42},
+		Settings: map[string]any{"key1": "value1", "key2": 42},
 	}
 	testCircular = func() *CircularStruct {
 		c := &CircularStruct{ID: 1, Name: "circular"}
@@ -78,7 +78,7 @@ var (
 
 func init() {
 	// Initialize test data
-	for i := 0; i < 100; i++ {
+	for i := range 100 {
 		testSlice[i] = i
 		testMap[string(rune('a'+i%26))+string(rune('a'+(i/26)%26))] = i
 	}
@@ -86,100 +86,100 @@ func init() {
 
 // Benchmark: Int
 func BenchmarkInt_This(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testInt)
 	}
 }
 
 func BenchmarkInt_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testInt)
 	}
 }
 
 func BenchmarkInt_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testInt)
 	}
 }
 
 func BenchmarkInt_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testInt)
 	}
 }
 
 // Benchmark: String
 func BenchmarkString_This(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testString)
 	}
 }
 
 func BenchmarkString_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testString)
 	}
 }
 
 func BenchmarkString_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testString)
 	}
 }
 
 func BenchmarkString_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testString)
 	}
 }
 
 // Benchmark: Float64
 func BenchmarkFloat_This(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testFloat)
 	}
 }
 
 func BenchmarkFloat_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testFloat)
 	}
 }
 
 func BenchmarkFloat_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testFloat)
 	}
 }
 
 func BenchmarkFloat_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testFloat)
 	}
 }
 
 // Benchmark: Bool
 func BenchmarkBool_This(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testBool)
 	}
 }
 
 func BenchmarkBool_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testBool)
 	}
 }
 
 func BenchmarkBool_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testBool)
 	}
 }
 
 func BenchmarkBool_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testBool)
 	}
 }
@@ -187,25 +187,25 @@ func BenchmarkBool_GolangDesign(b *testing.B) {
 // Benchmark: Slice (100 ints)
 func BenchmarkSlice100_This(b *testing.B) {
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testSlice)
 	}
 }
 
 func BenchmarkSlice100_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testSlice)
 	}
 }
 
 func BenchmarkSlice100_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testSlice)
 	}
 }
 
 func BenchmarkSlice100_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testSlice)
 	}
 }
@@ -213,25 +213,25 @@ func BenchmarkSlice100_GolangDesign(b *testing.B) {
 // Benchmark: Map (100 entries)
 func BenchmarkMap100_This(b *testing.B) {
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testMap)
 	}
 }
 
 func BenchmarkMap100_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testMap)
 	}
 }
 
 func BenchmarkMap100_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testMap)
 	}
 }
 
 func BenchmarkMap100_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testMap)
 	}
 }
@@ -239,32 +239,32 @@ func BenchmarkMap100_GolangDesign(b *testing.B) {
 // Benchmark: Simple Struct
 func BenchmarkSimpleStruct_This(b *testing.B) {
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testSimple)
 	}
 }
 
 func BenchmarkSimpleStruct_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testSimple)
 	}
 }
 
 func BenchmarkSimpleStruct_Jinzhu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		var dst SimpleStruct
 		_ = copier.Copy(&dst, &testSimple)
 	}
 }
 
 func BenchmarkSimpleStruct_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testSimple)
 	}
 }
 
 func BenchmarkSimpleStruct_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testSimple)
 	}
 }
@@ -272,32 +272,32 @@ func BenchmarkSimpleStruct_GolangDesign(b *testing.B) {
 // Benchmark: Nested Struct
 func BenchmarkNestedStruct_This(b *testing.B) {
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testNested)
 	}
 }
 
 func BenchmarkNestedStruct_Mohae(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(testNested)
 	}
 }
 
 func BenchmarkNestedStruct_Jinzhu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		var dst NestedStruct
 		_ = copier.CopyWithOption(&dst, &testNested, copier.Option{DeepCopy: true})
 	}
 }
 
 func BenchmarkNestedStruct_Huandu(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(testNested)
 	}
 }
 
 func BenchmarkNestedStruct_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testNested)
 	}
 }
@@ -305,84 +305,84 @@ func BenchmarkNestedStruct_GolangDesign(b *testing.B) {
 // Benchmark: Pointers
 func BenchmarkPointer_This(b *testing.B) {
 	ptr := &testSimple
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(ptr)
 	}
 }
 
 func BenchmarkPointer_Mohae(b *testing.B) {
 	ptr := &testSimple
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(ptr)
 	}
 }
 
 func BenchmarkPointer_Huandu(b *testing.B) {
 	ptr := &testSimple
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(ptr)
 	}
 }
 
 func BenchmarkPointer_GolangDesign(b *testing.B) {
 	ptr := &testSimple
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(ptr)
 	}
 }
 
 // Benchmark: Circular Reference
 func BenchmarkCircular_This(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(testCircular)
 	}
 }
 
 // mohae/deepcopy doesn't handle circular references properly - causes stack overflow
 // func BenchmarkCircular_Mohae(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
+// 	for b.Loop() {
 // 		_ = deepcopy.Copy(testCircular)
 // 	}
 // }
 
 // huandu/go-clone also has issues with circular references
 // func BenchmarkCircular_Huandu(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
+// 	for b.Loop() {
 // 		_ = clone.Clone(testCircular)
 // 	}
 // }
 
 func BenchmarkCircular_GolangDesign(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(testCircular)
 	}
 }
 
 // Benchmark: Interface
 func BenchmarkInterface_This(b *testing.B) {
-	var iface interface{} = testSimple
-	for i := 0; i < b.N; i++ {
+	var iface any = testSimple
+	for b.Loop() {
 		_ = deepclone.Clone(iface)
 	}
 }
 
 func BenchmarkInterface_Mohae(b *testing.B) {
-	var iface interface{} = testSimple
-	for i := 0; i < b.N; i++ {
+	var iface any = testSimple
+	for b.Loop() {
 		_ = deepcopy.Copy(iface)
 	}
 }
 
 func BenchmarkInterface_Huandu(b *testing.B) {
-	var iface interface{} = testSimple
-	for i := 0; i < b.N; i++ {
+	var iface any = testSimple
+	for b.Loop() {
 		_ = clone.Clone(iface)
 	}
 }
 
 func BenchmarkInterface_GolangDesign(b *testing.B) {
-	var iface interface{} = testSimple
-	for i := 0; i < b.N; i++ {
+	var iface any = testSimple
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(iface)
 	}
 }
@@ -395,7 +395,7 @@ func BenchmarkLargeSlice_This(b *testing.B) {
 	}
 	b.ReportAllocs()
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepclone.Clone(largeSlice)
 	}
 }
@@ -406,7 +406,7 @@ func BenchmarkLargeSlice_Mohae(b *testing.B) {
 		largeSlice[i] = i
 	}
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = deepcopy.Copy(largeSlice)
 	}
 }
@@ -417,7 +417,7 @@ func BenchmarkLargeSlice_Huandu(b *testing.B) {
 		largeSlice[i] = i
 	}
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = clone.Clone(largeSlice)
 	}
 }
@@ -428,14 +428,14 @@ func BenchmarkLargeSlice_GolangDesign(b *testing.B) {
 		largeSlice[i] = i
 	}
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = reflectpkg.DeepCopy(largeSlice)
 	}
 }
 
 // JSON vs Deep Clone comparison
 func BenchmarkJSON_Marshal_Unmarshal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		data, _ := json.Marshal(testNested)
 		var dst NestedStruct
 		_ = json.Unmarshal(data, &dst)
@@ -444,12 +444,12 @@ func BenchmarkJSON_Marshal_Unmarshal(b *testing.B) {
 
 // Reflection-based manual clone
 func BenchmarkReflection_Manual(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		_ = manualReflectClone(testNested)
 	}
 }
 
-func manualReflectClone(src interface{}) interface{} {
+func manualReflectClone(src any) any {
 	srcVal := reflect.ValueOf(src)
 	return reflect.New(srcVal.Type()).Elem().Interface()
 }
--- benchmarks/go.mod
+++ benchmarks/go.mod
@@ -1,6 +1,6 @@
 module github.com/kaptinlin/deepclone/benchmarks
 
-go 1.25
+go 1.26
 
 require (
 	github.com/go-json-experiment/json v0.0.0-20250910080747-cc2cfa0554c3
